eb4b45bf232ee2df82a66e5a69ca5565e7329857
Michael Bestas <mkbestas@lineageos.org>
2024-07-08 08:56:25 +0300

    sm6475-common: Remove media_codecs_google_c2*
    AOSP codecs can be used by default and it is not necessary to include
    them device side.
    
    Change-Id: I8227b06f0466b614f12fa9008402b92461ea3dd0

--- extract-files.py
+++ extract-files.py
@@ -92,7 +92,7 @@ blob_fixups: blob_fixups_user_type = {
        'vendor/etc/media_codecs_parrot_v0.xml',
        'vendor/etc/media_codecs_ravelin.xml',
     ): blob_fixup()
-        .regex_replace('.+media_codecs_(google_audio|google_telephony|vendor_audio).+\n', ''),
+        .regex_replace('.+media_codecs_(google_audio|google_c2|google_telephony|vendor_audio).+\n', ''),
 } # fmt: skip
 
 module = ExtractUtilsModule(
