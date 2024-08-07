ab29c158c9768f6b0202b904c62c2a52f1fbb65c
Cosmin Tanislav <demonsingur@gmail.com>
2024-08-08 00:41:46 +0300

    sm6475-common: Import sensors from sm8475-common
    Change-Id: I1cf83b496dfc26f3860ff7584b36f79dd655e7a6

--- common.mk
+++ common.mk
@@ -243,7 +243,8 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors-service.multihal
+    android.hardware.sensors-service.multihal \
+    sensors.moto_sm6475:64
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
