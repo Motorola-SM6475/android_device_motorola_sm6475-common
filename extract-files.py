d8e9f40c9bbf513a58ba9f4fbdfcc96fe4132318
Bruno Martins <bgcngm@gmail.com>
2024-10-01 14:42:47 +0100

    sm6475-common: Resolve missing symbols on libqcodec2_core
    Change-Id: I300d78733f07b7fe1ba6b5066d02f49eefbeb54a

--- extract-files.py
+++ extract-files.py
@@ -79,7 +79,9 @@ blob_fixups: blob_fixups_user_type = {
         )
     'vendor/lib64/vendor.libdpmframework.so': blob_fixup()
         .add_needed('libhidlbase_shim.so'),
-}  # fmt: skip
+    'vendor/lib64/libqcodec2_core.so': blob_fixup()
+        .add_needed('libcodec2_shim.so'),
+} # fmt: skip
 
 module = ExtractUtilsModule(
     'sm6475-common',
