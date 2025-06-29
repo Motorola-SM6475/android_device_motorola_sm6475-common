bb007011cc444cee036ce6b0b5f0fae9c82b8974
Nikita Savchenko <NikitaSavc@yandex.ru>
2025-06-29 15:49:11 +0300

    ыsm6475-common: Initial configuration
    Change-Id: Ie30473077ee50835b1c5f9363dc7965245fce64c

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,41 @@
+#
+# Copyright (C) 2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+COMMON_PATH := device/motorola/sm6475-common
+
+# A/B
+AB_OTA_PARTITIONS += \
+    boot \
+    dtbo \
+    init_boot \
+    odm \
+    product \
+    recovery \
+    system \
+    system_dlkm \
+    system_ext \
+    vbmeta \
+    vbmeta_system \
+    vendor \
+    vendor_boot \
+    vendor_dlkm
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-2a-dotprod
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := kryo385
+
+# Bootloader
+TARGET_NO_BOOTLOADER := true
+
+# Platform
+BOARD_USES_QCOM_HARDWARE := true
+TARGET_BOARD_PLATFORM := parrot
+
+# Inherit the proprietary files
+include vendor/motorola/sm6475-common/BoardConfigVendor.mk
