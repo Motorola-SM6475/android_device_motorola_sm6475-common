42423f738914f5c937831df21065b14b916aebcf
Nikita Savchenko <NikitaSavc@yandex.ru>
2025-06-29 21:25:53 +0000

    sm6475-common: Fix extract-files.py running
    Change-Id: Ib5e315d6bec43523c9ca6e26efb79d635d968a8d

--- extract-files.py
+++ extract-files.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
 #
 # SPDX-FileCopyrightText: 2025 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
@@ -12,7 +13,9 @@ from extract_utils.fixups_lib import (
     lib_fixups,
     lib_fixups_user_type,
 )
+
 from extract_utils.main import (
+    ExtractUtils,
     ExtractUtilsModule,
 )
 
@@ -57,3 +60,7 @@ module = ExtractUtilsModule(
     lib_fixups=lib_fixups,
     namespace_imports=namespace_imports,
 )
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()
