e2bbf759ba3874813fb9a4fd9087596a31a64b7c
Nikita Savchenko <NikitaSavc@yandex.ru>
2025-06-30 08:59:40 +0000

    sm6475-common: Build update engine
    Change-Id: I8159e89857d31a854ae325b582bc2cd439e985b4

--- common.mk
+++ common.mk
@@ -125,5 +125,11 @@ PRODUCT_PACKAGES += \
     IPACM_cfg.xml \
     IPACM_Filter_cfg.xml
 
+# Update engine
+PRODUCT_PACKAGES += \
+    update_engine \
+    update_engine_sideload \
+    update_verifier
+
 # Inherit from vendor blobs
 $(call inherit-product, vendor/motorola/sm6475-common/sm6475-common-vendor.mk)
