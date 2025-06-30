81ebab83c636fb68e05ff37ba536f5aaf43c3fe3
Nikita Savchenko <NikitaSavc@yandex.ru>
2025-06-30 08:53:06 +0000

    sm6475-common: Build IPACM
    Change-Id: Iadfce6630f37cc0d3b634a553269cf2e338bc6fc

--- common.mk
+++ common.mk
@@ -116,5 +116,11 @@ PRODUCT_PACKAGES += \
     android.hardware.health-service.qti \
     android.hardware.health-service.qti_recovery
 
+# IPACM
+PRODUCT_PACKAGES += \
+    ipacm \
+    IPACM_cfg.xml \
+    IPACM_Filter_cfg.xml
+
 # Inherit from vendor blobs
 $(call inherit-product, vendor/motorola/sm6475-common/sm6475-common-vendor.mk)
