f1dff9998465b949b61475cdca1ac13a60aa39a4
Nikita Savchenko <NikitaSavc@yandex.ru>
2025-06-30 08:59:40 +0000

    sm6475-common: Build update engine
    Change-Id: I8159e89857d31a854ae325b582bc2cd439e985b4

--- common.mk
+++ common.mk
@@ -121,5 +121,14 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libpalclient:64
 
+# Update engine
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client
+
+PRODUCT_PACKAGES += \
+    update_engine \
+    update_engine_sideload \
+    update_verifier
+
 # Inherit from vendor blobs
 $(call inherit-product, vendor/motorola/sm6475-common/sm6475-common-vendor.mk)
