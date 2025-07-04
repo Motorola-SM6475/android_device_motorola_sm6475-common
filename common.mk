43f88e7f20ffbcac419d4e40bd111fbfac6705f9
Nikita Savchenko <NikitaSavc@yandex.ru>
2025-07-04 16:26:35 +0000

    sm6475-common: Copy nfc mifare permission
    Change-Id: I2f028570b1f550603a0921ca404ddb2d0d74f66a

--- common.mk
+++ common.mk
@@ -185,6 +185,9 @@ PRODUCT_PACKAGES += \
     nqnfcinfo \
     Tag
 
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
+
 # Overlay
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
