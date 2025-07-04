a5265a049b14c7475701ea53e9c334cb62cf54b3
AMWolfstein <76236780+AMWolfstein@users.noreply.github.com>
2025-07-04 04:11:04 +0300

    sm6475-common: Setup partitions
    Change-Id: Ia96d43cf016a2345d1a48c649fbd46537af14743

--- common.mk
+++ common.mk
@@ -160,6 +160,10 @@ PRODUCT_PACKAGES += \
     FrameworksResTarget \
     WifiResTarget
 
+# Partitions
+PRODUCT_BUILD_SUPER_PARTITION := false
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
