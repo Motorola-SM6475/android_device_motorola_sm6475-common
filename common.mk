5d3d5c11297d9c0213f6d696ad9909cc8e8aaf1f
Nikita Savchenko <NikitaSavc@yandex.ru>
2025-07-04 16:26:53 +0000

    sm6475-common: Copy euicc telephony permission to product
    Change-Id: Ib3fb78632f0f7bac4ce4724f8205cc27a18cfa91

--- common.mk
+++ common.mk
@@ -255,6 +255,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
 
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
+
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal-service.qti
