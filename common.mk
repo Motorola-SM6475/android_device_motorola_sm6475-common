542ef404071e44583dd09cfab7d018141bd141a2
AMWolfstein <ahmedhero4040@gmail.com>
2025-07-12 00:41:57 +0300

    sm6475-common: Add public libraries from stock
    * From paros:15/SUQ32HV-V1-ST14/56b832:user/release-keys
    
    Change-Id: I03815a25c0cc186c0fb3054c8795f775147641cd

--- common.mk
+++ common.mk
@@ -229,6 +229,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
+# Native libraries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt \
+    $(LOCAL_PATH)/configs/public.libraries-qti.txt:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/public.libraries-qti.txt
+
 # Overlay
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
