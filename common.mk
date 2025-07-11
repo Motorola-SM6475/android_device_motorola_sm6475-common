5a87d91bf6a8c60cca6a93bfb0a7a48681ab2ba2
AMWolfstein <ahmedhero4040@gmail.com>
2025-07-12 00:41:57 +0300

    sm6475-common: Add public libraries from stock
    * From paros:15/SUQ32HV-V1-ST14/56b832:user/release-keys

--- common.mk
+++ common.mk
@@ -174,6 +174,11 @@ PRODUCT_PACKAGES += \
     vendor_super_fsg_mountpoint \
     vendor_super_modem_mountpoint
 
+# Native libraries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt \
+    $(LOCAL_PATH)/configs/public.libraries-qti.txt:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/public.libraries-qti.txt
+
 # Overlay
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
