7173ad49c508c9ee30fd9583d9f1ca8a2f36c0d9
AMWolfstein <ahmedhero4040@gmail.com>
2025-07-12 00:48:53 +0300

    sm6475-common: Import media files from stock
    * From paros:15/SUQ32HV-V1-ST14/56b832:user/release-keys

--- common.mk
+++ common.mk
@@ -165,6 +165,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libpalclient:64
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/media/media_codecs_performance_parrot_v0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_parrot_v0.xml \
+    $(LOCAL_PATH)/configs/media/media_codecs_parrot_v0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_parrot_v0.xml \
+    $(LOCAL_PATH)/configs/media/media_profiles_parrot_v0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_parrot_v0.xml
+
 # Mountpoints
 PRODUCT_PACKAGES += \
     vendor_bt_firmware_mountpoint \
